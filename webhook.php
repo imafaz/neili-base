d99d19bc0be2cdb87a13961262863b0571fd30bd
unknown <abolfazlmajidi100@gmail.com>
2024-11-14 18:11:27 +0330

    auto commit

--- webhook.php
+++ webhook.php
@@ -26,6 +26,11 @@ if ($update) {
 
 if ($chatType == 'private') {
     if ($text == '/start') {
-        $bot->sendMessage($chatId, $_LANG['startmessage']);
+        $buttons = ['button1', 'button2', 'button3', 'button4', 'button5'];
+        $keyboard = Neili::keyboard($buttons);
+        $bot->sendMessage($chatId, $_LANG['startmessage'], $keyboard);
+        $buttons = ['button1' => 'customdata', 'button2' => 'customdata', 'button3' => 'customdata'];
+        $keyboard = Neili::inlineKeyboard($buttons);
+        $bot->sendMessage($chatId, $_LANG['startmessage'], $keyboard);
     }
 }
